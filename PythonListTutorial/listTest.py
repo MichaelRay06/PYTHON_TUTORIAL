a4f36419c784ea0eb627302e545159502ea385a0
MichaelRay06 <michaelo.ray06@gmail.com>
2025-03-03 12:22:42 +0000

    Learning python list

---
+++ PythonListTutorial/listTest.py
@@ -0,0 +1,13 @@
+list_indexing = ["london" , "Lagos", "New York", "Toronto", "Paris"]
+
+print( list_indexing[3].upper())
+newList= list_indexing[3].upper()
+print(newList.isupper())
+
+
+if len(list_indexing[0]) > len(list_indexing[2]):
+     print("The  first is the longest")
+else:
+     print (" not the longest")
+
+
